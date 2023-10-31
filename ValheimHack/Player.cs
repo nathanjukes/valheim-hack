811ebc2a209994db98881bea96e2bcde66010517
jakepwheeler <jakephilipwheeler@gmail.com>
2023-10-31 16:44:52 +0000

    initial example of ghost and god mode class in DLL format

---
+++ ValheimHack/Player.cs
@@ -0,0 +1,34 @@
+﻿﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ValheimHack
+{
+    /// <summary>
+    /// Turn on god mode and ghost mode
+    /// </summary>
+
+     [HarmonyPatch(typeof(Player), nameof(Player.InGodMode))]
+     public static class Player_InGodMode_patch
+     {
+        private static override bool Postfix(ref Player __instance, ref bool __result)
+        {
+            __result = true;
+	        return true;
+        }
+     }
+
+     [HarmonyPatch(typeof(Player), nameof(Player.InGhostMode))]
+     public static class Player_InGhostMode_patch
+     {
+        private static override bool Postfix(ref Player __instance, ref bool __result)
+        {
+            __result = true;
+	        return true;
+        }
+     }
+}
